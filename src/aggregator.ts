b4c2118d406fb3b7ff9d3f1603c528cbd518b6e6
Neriya Rosner <neri.coder@gmail.com>
2023-05-16 14:32:14 +0300

    fix aggregator

--- src/aggregator.ts
+++ src/aggregator.ts
@@ -1,20 +1,18 @@
-import {AggregationMethod, Event} from "@/lib/entities/metric_event";
+import {AGGREGATION_OPERATOR, AggregationMethod, Event} from "@/lib/entities/metric_event";
 
 export function aggregate(method: AggregationMethod, events: Event[]): number | null {
     const data = events.map(event => event.data[method.field]);
 
-    try {
-        switch (method.operator) {
-            case "average":
-                return data.reduce((a, b) => a + b, 0) / data.length;
-            case "sum":
-                return data.reduce((a, b) => a + b, 0);
-            case "min":
-                return Math.min(...data);
-            case "max":
-                return Math.max(...data);
-        }
-    } catch (e) {
-        return null
+
+    switch (method.operator) {
+        case AGGREGATION_OPERATOR.AVERAGE:
+            return data.reduce((a, b) => a + b, 0) / data.length;
+        case AGGREGATION_OPERATOR.SUM:
+            return data.reduce((a, b) => a + b, 0);
+        case AGGREGATION_OPERATOR.MIN:
+            return Math.min(...data);
+        case AGGREGATION_OPERATOR.MAX:
+            return Math.max(...data);
     }
+
 }
