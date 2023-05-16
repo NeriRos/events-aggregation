b97e39d0b592a646c2dc9570d95b828c94db8190
Neriya Rosner <neri.coder@gmail.com>
2023-05-16 12:38:05 +0300

    create repository abstration

---
+++ src/repositories/events_repository.ts
@@ -0,0 +1,11 @@
+import {Events, EventTypes} from "@/events";
+import {Event} from "@/lib/entities/metric_event";
+
+export abstract class EventsRepository {
+    protected constructor(protected events: Events, protected eventType: EventTypes) {
+    }
+
+    async create(item: Event): Promise<boolean> {
+        return this.events.publish(this.eventType, item);
+    }
+}
