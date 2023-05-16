ab95ff1362ba7788cbdad25d6bc2f5f279ec0a39
Neriya Rosner <neri.coder@gmail.com>
2023-05-16 12:04:23 +0300

    reroute aliases for jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+    transform: {'^.+\\.ts?$': 'ts-jest'},
+    testEnvironment: 'node',
+    testRegex: '/src/.*\\.(test|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    'moduleNameMapper': {
+        '@/(.*)': '<rootDir>/src/$1',
+        '@/lib/(.*)': '<rootDir>/lib/$1',
+        '@/data/(.*)': '<rootDir>/data/$1',
+    }
+};
