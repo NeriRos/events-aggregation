8f4dbc8923ba33a7717fe5321016b9d9a9694527
Neriya Rosner <neri.coder@gmail.com>
2023-05-16 12:54:17 +0300

    run the flow

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import {Events, EventTypes} from "@/events";
-
-console.log('Hello World')
+import {LocalRepository} from "@/repositories/local_repository";
 
 const events: Events = new Events();
+const localRepository: LocalRepository = new LocalRepository(events, EventTypes.metric, `${process.cwd()}/data/events.json`);
 
 events.subscribe(EventTypes.metric, (data: any) => {
-
+    console.log(data)
 });
+
+localRepository.createEvents();
