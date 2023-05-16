9ef3d4c1acd12b530ea883521b17dc0e94beee43
Neriya Rosner <neri.coder@gmail.com>
2023-05-16 13:33:25 +0300

    implement load

--- lib/repositories/local_repository.ts
+++ lib/repositories/local_repository.ts
@@ -1,32 +1,29 @@
 import {Events} from "@/events";
 import {Event} from "@/lib/entities/metric_event";
-import {EventsRepository} from "@/repositories/events_repository";
 import * as fs from "fs";
+import {EventsRepository} from "@/lib/repositories/events_repository";
 
 export class LocalRepository extends EventsRepository {
     constructor(events: Events, private filePath: string) {
         super(events);
     }
 
-    async createEvents() {
-        const events = await this.loadFile();
-
-        events.forEach(event => {
-            this.create(event);
-        });
-    }
-
-    private async loadFile(): Promise<Event[]> {
-        const promise = new Promise<Event[]>((resolve, reject) => {
+    async load(): Promise<Event[] | Error> {
+        return new Promise((resolve, reject) => {
             fs.readFile(this.filePath, (err, data) => {
                 if (err) {
                     console.error(err);
                     reject(err);
                 }
-                resolve(JSON.parse(data.toString()));
-            });
-        })
 
-        return await promise;
+                const events: Event[] = JSON.parse(data.toString())
+
+                events.forEach(event => {
+                    this.create(event);
+                });
+
+                resolve(events);
+            });
+        });
     }
 }
